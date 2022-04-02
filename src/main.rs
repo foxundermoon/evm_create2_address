2926436510494d5e8958c886c007456cec913c64
fox <hi@fox.mn>
2022-04-02 16:27:03 +0800

    cal address by regex

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,20 @@ fn main() {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+		if let Err(e) = create2crunch::apollo(config) {
+			eprintln!("CPU apollo application error: {}", e);
+			process::exit(1);
+	}
 
-    if config.gpu_device == 255 {
-	    if let Err(e) = create2crunch::cpu(config) {
-	        eprintln!("CPU application error: {}", e);
-	        process::exit(1);
-	    }
-    } else {
-	    if let Err(e) = create2crunch::gpu(config) {
-	        eprintln!("GPU application error: {}", e);
-	        process::exit(1);
-	    }
-    }
+    // if config.gpu_device == 255 {
+	  //   if let Err(e) = create2crunch::cpu(config) {
+	  //       eprintln!("CPU application error: {}", e);
+	  //       process::exit(1);
+	  //   }
+    // } else {
+	  //   // if let Err(e) = create2crunch::gpu(config) {
+	  //   //     eprintln!("GPU application error: {}", e);
+	  //   //     process::exit(1);
+	  //   // }
+    // }
 }
